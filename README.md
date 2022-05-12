e723aa3a5d6a1b1b89523082a52844f320199819
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-05-12 17:15:03 +0300

    readme added
    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an website that aims to understand the public approach to trend topics.
+
+You can give vote via link below!!
+
+www.examplelink.com
--- frontend/src/components/mainPage/rootPage.js
+++ frontend/src/components/mainPage/rootPage.js
@@ -1,13 +1,12 @@
 
 import "./root.css"
-import { useState } from "react";
 import Question from "../Question";
 
 function Main() {
 
     return <div className="main-content">
 
-        {Question({question: "What do you think about some topic", options: ["Yes", "No"]})}
+        {Question({question: "What do you think about some topic ?", options: ["Yes", "No"]})}
 
         <button className="question-submit" type="submit">
             Submit
