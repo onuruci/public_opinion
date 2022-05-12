d9879977422744621ee4ae8a03c25aba7691bc36
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-05-12 14:38:16 +0300

    main-content changes
    question and options are added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+
+import Main from './mainPage/rootPage'
 import Navbar from './components/Navbar';
 
 function App() {
@@ -12,10 +14,11 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<div>A</div>} />
+          <Route path="/" element={<Main></Main>} />
           <Route path="all" element={<div>A</div>} />
           <Route path="admin" element={<div>A</div>} />
         </Routes>
+        
       </BrowserRouter>
     </div>
   );
--- src/mainPage/rootPage.js
+++ src/mainPage/rootPage.js
@@ -1,11 +1,28 @@
 
+import "./root.css"
 
 function Main(){
 
-    return <div>
+    return <div className="main-content">
 
-        Ne düşünüyorsunuz
+        <div className="question">
+            What do you think about some topic ?
+        </div>
 
+        <div className="options">
+            <button className="option">
+                Option 1
+            </button>
+
+            <button className="option">
+                Option 2
+            </button>
+        </div>    
+        <button type="submit">
+                Submit
+        </button>
     </div>
 
 }
+
+export default Main
