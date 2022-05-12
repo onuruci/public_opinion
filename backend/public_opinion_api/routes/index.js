56b596d4078f9da38a1282508ba0b093c41b2be5
onuruci <onur.yuce@metu.edu.tr>
2022-05-12 17:43:19 +0300

    routes skeleton

--- backend/public_opinion_api/routes/index.js
+++ backend/public_opinion_api/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ status: 'Entered' });
+});
+
+router.get('/most_popular', function(req, res, next){
+  res.send({status: 'most_popular_question'});
+});
+
+router.get('/all_questions', function(req, res, next){
+  res.send({status: 'all_questions'});
+});
+
+router.get('/admin_auth', function(req, res, next){
+  res.send({status: 'admin_auth'});
+});
+
+router.get('/admin_content', function(req, res, next){
+  res.send({status: 'admin_auth'});
 });
 
 module.exports = router;
